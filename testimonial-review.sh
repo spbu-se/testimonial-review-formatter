e31100a09e64bfd5852cd7af472ba5e16a8fbcb1
Iakov 'Jake' Kirilenko <Jake.Kirilenko@gmail.com>
2019-05-18 17:16:35 +0300

    Handle spaces in path/name

--- testimonial-review.sh
+++ testimonial-review.sh
@@ -1,22 +1,22 @@
 #!/bin/bash
 set -ueo pipefail
 
-here=`dirname $0`
+here=$(dirname $0)
 
 # if your OS is sick
 here=${here//\\/\/}
 
 test $# -gt 0 ||  { echo -e "Missing input file.\nUsage:\n\t$0 <markdown-input-file>\n" ;  exit 0 ; }
 
-src=$1
-srcdir=`dirname $src`
-srcdir=${srcdir//\\/\/}
+src="$1"
+srcdir=$(dirname "$src")
+srcdir="${srcdir//\\/\/}"
 
-tex=${src%.*}.tex
-txt=${src%.*}.txt
+tex="${src%.*}.tex"
+txt="${src%.*}.txt"
 
-pandoc -t latex -o $tex -V here=$here --template=${here}/templates/testimonial.latex $src
-pandoc -t plain -o $txt --template=${here}/templates/testimonial.plain $src
+pandoc -t latex -o "$tex" -V here="$here" --template "${here}/templates/testimonial.latex" "$src"
+pandoc -t plain -o "$txt" --template "${here}/templates/testimonial.plain" "$src"
 
-xelatex -output-directory=$srcdir $tex
-xelatex -output-directory=$srcdir $tex
+xelatex -output-directory "$srcdir" "$tex"
+xelatex -output-directory "$srcdir" "$tex"
